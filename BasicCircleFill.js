a4190d7b415467cab96307e187c6dc0dfdaf3dfb
amansharma193 <72331702+amansharma193@users.noreply.github.com>
2021-12-11 12:03:07 +0530

    Create BasicCircleFill.js

---
+++ BasicCircleFill.js
@@ -0,0 +1,13 @@
+function setup() {
+	simple();
+}
+
+function draw() {
+	// circle(left, top, size)
+	fill('lightgreen');
+	circle(400,300,300);
+	fill('blue');
+	circle(400,300,200);
+	fill('red');
+	circle(400,300,100);
+}
