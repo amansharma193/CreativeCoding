51a5e4c2cca86a5020f9d24d9f796f00a34135e0
amansharma193 <72331702+amansharma193@users.noreply.github.com>
2021-12-12 10:49:48 +0530

    Create SinWave.js

---
+++ SinWave.js
@@ -0,0 +1,40 @@
+function setup() {
+	simple();
+}
+
+function draw() {
+	strokeWeight(4);	
+	noFill();
+	let x=100;
+	for(let j=0;j<40;j++){
+		for(let i=0;i<=800;i+=10){
+			balloon(i,x);
+		}
+			x+=10;
+	}
+	// repeat(0,800,10,balloon);
+	// repeat(0,800,10,balloon2);
+	// repeat(0,800,10,balloon3);
+}
+
+function balloon(x,y){
+	// let red=randomNumber(256);
+	// let blue=randomNumber(256);
+	// let green=randomNumber(256);
+	stroke(x/2,x/4,128);
+	circle(x,y+sin(x)*100,4);
+}
+function balloon2(x){
+	// let red=randomNumber(256);
+	// let blue=randomNumber(256);
+	// let green=randomNumber(256);
+	stroke(x/2,x/4,128);
+	circle(x,290+sin(x)*100,4);
+}
+function balloon3(x){
+	// let red=randomNumber(256);
+	// let blue=randomNumber(256);
+	// let green=randomNumber(256);
+	stroke(x/2,x/4,128);
+	circle(x,280+sin(x)*100,4);
+}
